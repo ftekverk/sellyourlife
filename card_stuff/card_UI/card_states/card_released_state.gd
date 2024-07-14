599eae47446ac04f7c345df55e2bfbcdda08cf2d
Finn <ftekverk@gmail.com>
2024-07-14 17:09:45 -0400

    game handler connected

--- card_stuff/card_UI/card_states/card_released_state.gd
+++ card_stuff/card_UI/card_states/card_released_state.gd
@@ -5,8 +5,6 @@ var played: bool
 
 signal card_played
 
-@onready var game_handler = $GameHandler
-
 
 
 func enter() -> void:
@@ -18,25 +16,7 @@ func enter() -> void:
 		played = true
 	else:
 		return
-		
-#	Get attributes of played card
-	print("Played Card Value: ", card_ui.card.value)
-	print("Played Card Suit: ", card_ui.card.suit)
-
-	print("emitting card played")
-	card_played.emit()
-	
-#	Generate a signal to the game handler that a card has been played
-
-
-
-
-#	1.) Get a card from the dealer and determine if win
-#	var dealer_card = generate_dealer_card()
-#	var win = handle_card_game(card_ui.card, dealer_card)
-
-#	2.) Handle betting
-#	handle_betting()
+	card_played.emit(card_ui)
 
 
 
--- scripts/game_handler.gd
+++ scripts/game_handler.gd
@@ -12,28 +12,57 @@ extends Node2D
 
 # Important variable
 var dealer_hand := []
+var dealer_hand_paths := []  # used for debugging
 var player_hand := []
 
 
 func _ready():
 	
 #	1.) Connect the card_played signal to handle_card_play
+	if not hand or not dealer:
+		return
+		
 	for CardUI in hand.get_children():
 		var released_state = CardUI.get_node("CardStateMachine").get_node("CardReleasedState")
 		released_state.connect("card_played", handle_card_play)
 
 #	2.) Get important variables
 	dealer_hand = dealer.dealer_hand
-	#player_hand = hand.
-
-
-func handle_card_play():
-	print("handling card play!")
+	dealer_hand_paths = dealer.dealer_hand_paths
+	player_hand = hand.selected_cards
 	
-#	Choose dealer card
 
 
-#	Check if player won
+func handle_card_play(player_card_ui):
+	print("handling card play!")
+
+	var player_card = player_card_ui.card
+
+	print("Played Card Value: ", player_card.value)
+	print("Played Card Suit: ", player_card.suit)
+	
+
+#	1.) Get a card from the dealer and determine if win
+	var dealer_card = dealer.choose_card_to_play(dealer_hand)
+	
+	print("dealer_card value: ", dealer_card.card.value)
+	
+	
+	
+	#print(dealer_card)
+	#print("Played Card Value: ", dealer_card.card.value)
+
+
+
+
+
+#	var dealer_card = generate_dealer_card()
+#	var win = handle_card_game(card_ui.card, dealer_card)
+
+
+#	2.) Handle betting
+#	handle_betting()
+
 
 
 func did_player_win(player_card, dealer_card) -> bool:
